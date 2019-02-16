4a2b9e320efc5a32f6db9bcec0a001fb0023325c
huangkai <18778335525@163.com>
2019-02-16 16:14:37 +0800

    添加博客文章model

--- article/models.py
+++ article/models.py
@@ -1,5 +1,21 @@
+from datetime import datetime
+
 from django.db import models
 
 
 # Create your models here.
+class Article(models.Model):
+    """
+    博客-文章
+    """
+    title = models.CharField('文章标题', max_length=100, help_text='文章标题', unique=True)
+    body = models.TextField('文章内容')
+    create_time = models.DateTimeField('发布时间', default=datetime.now)
+    update_time = models.DateTimeField('修改时间', auto_now=True)
 
+    class Meta:
+        verbose_name = '博客文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
