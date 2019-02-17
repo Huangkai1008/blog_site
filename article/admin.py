a8d992c3b9b41c8008126386f8ed1e704df37601
huangkai <18778335525@163.com>
2019-02-17 15:31:06 +0800

    添加博客文章标签和分类

--- article/admin.py
+++ article/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article, Tag, Category
 
 
 @admin.register(Article)
@@ -9,4 +9,20 @@ class ArticleAdmin(admin.ModelAdmin):
     """
     文章admin
     """
-    fields = ('title', 'body', 'create_time')
+    fields = ('title', 'body', 'create_time', 'category', 'tags')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    分类admin
+    """
+    fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    标签admin
+    """
+    fields = ('name',)
--- article/models.py
+++ article/models.py
@@ -2,7 +2,35 @@ from datetime import datetime
 
 from django.db import models
 
-__all__ = ['Article']
+__all__ = ['Article', 'Category', 'Tag']
+
+
+class Category(models.Model):
+    """
+    博客-分类
+    """
+    name = models.CharField('分类名称', max_length=100)
+
+    class Meta:
+        verbose_name = '博客分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """
+    博客-标签
+    """
+    name = models.CharField('标签名称', max_length=100)
+
+    class Meta:
+        verbose_name = '博客标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
@@ -14,6 +42,8 @@ class Article(models.Model):
     body = models.TextField('文章内容')
     create_time = models.DateTimeField('发布时间', default=datetime.now)
     update_time = models.DateTimeField('修改时间', auto_now=True)
+    category = models.ForeignKey(Category, verbose_name='文章分类', on_delete=models.CASCADE, default='')
+    tags = models.ManyToManyField(Tag, verbose_name='文章标签', blank=True)
 
     class Meta:
         verbose_name = '博客文章'
